808dc8898a7fb98d9989423d59a48767cf34808d
Paul Fontaine <fontainepaul36@gmail.com>
2024-11-20 18:22:16 -0500

    préparation pour le nouvel entrainement

--- aprentissage/training.py
+++ aprentissage/training.py
@@ -3,7 +3,7 @@ from aprentissage.transfer_learning import ResNet50_transfer_learning
 dataset_path = '../ASL_dataset/asl_alphabet_train/asl_alphabet_train'
 ResNet50_transfer_learning(
     dataset_path,
-    num_epochs=10,
+    num_epochs=7,
     learning_rate=0.001,
     batch_size=32
 )
