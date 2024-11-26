b9722a31db4bb1fe1a489f97d2eb05a36d666afd
Paul Fontaine <105644400+Paul-Fontaine@users.noreply.github.com>
2024-11-26 13:29:22 -0500

    Delete aprentissage directory

--- aprentissage/training.py
+++
@@ -1,9 +0,0 @@
-from aprentissage.transfer_learning import ResNet50_transfer_learning
-
-dataset_path = '../ASL_dataset/asl_alphabet_train/asl_alphabet_train'
-ResNet50_transfer_learning(
-    dataset_path,
-    num_epochs=7,
-    learning_rate=0.001,
-    batch_size=32
-)
